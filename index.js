ff83a0dc0056750f4c8e99ecaa2acf475c458db3
carolldsk <carolldsk@gmail.com>
2021-05-13 16:29:41 -0300

    teste

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true, args: [ '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36', '--disable-background-timer-throttling', '--disable-backgrounding-occluded-windows', '--disable-renderer-backgrounding' ] });
+  const browser = await puppeteer.launch({ headless: false, args: [ '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36', '--disable-background-timer-throttling', '--disable-backgrounding-occluded-windows', '--disable-renderer-backgrounding' ] });
   const page = await browser.newPage();
   // await page.setViewport({ width: 1920, height: 1080 });
+  
 
   await page.goto('https://instagram.com/accounts/login');
 
@@ -23,6 +24,8 @@ const fs = require('fs');
 
   await page.waitForTimeout(1000);
 
+
+
   page.on('response', async response => {
     if(response.url() === 'https://i.instagram.com/api/v1/feed/reels_media/?reel_ids=23456103'){
       await fs.writeFile('estadao.json', JSON.stringify(await response.json(), null, 2), err => {
@@ -34,6 +37,8 @@ const fs = require('fs');
       await browser.close();
     }
   });
+
+  await page.waitForTimeout(1000);
 
   await page.goto('https://instagram.com/stories/estadao');
 })();
