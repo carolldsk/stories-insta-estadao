5f4e914aa0f221161116c5c58e7b25bbcb25f927
carolldsk <carolldsk@gmail.com>
2021-06-30 16:51:28 -0300

    inserindo packege json e remoção de conflitos

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ require('dotenv').config();
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
-function filtraVideos(){
+function filtraVideos() {
   let rawdata = fs.readFileSync('estadao.json');
   let medias = JSON.parse(rawdata).reels_media[0].items;
 
   let saida = [];
   medias.forEach(item => {
-    if(item.video_versions){
+    if (item.video_versions) {
       saida.push({
-        videoUrl : item.video_versions[0].url,
+        videoUrl: item.video_versions[0].url,
         storyDuration: item.video_duration,
         mediaType: 0
       })
@@ -19,7 +19,7 @@ function filtraVideos(){
   return saida
 }
 
-function escreveJsonVideos(saida){
+function escreveJsonVideos(saida) {
   let data = JSON.stringify(saida, null, 2);
 
   fs.writeFile('videos.json', data, (err) => {
@@ -29,21 +29,17 @@ function escreveJsonVideos(saida){
 }
 
 (async () => {
-<<<<<<< HEAD
-  const browser = await puppeteer.launch({ headless: false, args: [ '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36', '--disable-background-timer-throttling', '--disable-backgrounding-occluded-windows', '--disable-renderer-backgrounding' ] });
-=======
-  const browser = await puppeteer.launch({ headless: true, args: [ '--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36', '--disable-background-timer-throttling', '--disable-backgrounding-occluded-windows', '--disable-renderer-backgrounding' ] });
+  const browser = await puppeteer.launch({ headless: false, args: ['--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36', '--disable-background-timer-throttling', '--disable-backgrounding-occluded-windows', '--disable-renderer-backgrounding'] });
   //const browser = await puppeteer.launch({ headless: false });
->>>>>>> 866d18de7166439d8b8d38721396fe7456dd472d
   const page = await browser.newPage();
   // await page.setViewport({ width: 1920, height: 1080 });
-  
+
 
   await page.goto('https://instagram.com/accounts/login');
 
   // Aguarda o input de usuário para preencher
   await page.waitForSelector('[name="username"]');
-  
+
   // Digita os dados nos inputs
   await page.type('[name="username"]', process.env.INSTAGRAM_EMAIL);
   await page.type('[name="password"]', process.env.INSTAGRAM_PASSWORD);
@@ -56,11 +52,29 @@ function escreveJsonVideos(saida){
   await page.waitForTimeout(1000);
 
 
+  await page.click('[type="submit"]');
+
+
 
   page.on('response', async response => {
-    if(response.url() === 'https://i.instagram.com/api/v1/feed/reels_media/?reel_ids=23456103'){
+    //let modalNotification = await page.$$("div[role='presentation'] > div[role='dialog'] .mt3GC button.HoLwm");
+
+    //console.log(response);
+
+    //if (modalNotification) {
+    //console.log('foi');
+
+    //await page.click('button.HoLwm');
+    //}
+    //else {
+    //console.log('AFFFFF !!');
+
+    //}
+    await page.waitForTimeout(1000);
+
+    if (response.url() === 'https://i.instagram.com/api/v1/feed/reels_media/?reel_ids=23456103') {
       await fs.writeFile('estadao.json', JSON.stringify(await response.json(), null, 2), err => {
-        if(err) throw new Error(err);
+        if (err) throw new Error(err);
 
 
         console.log('JSON file generated successfully!');
@@ -68,7 +82,7 @@ function escreveJsonVideos(saida){
 
       await browser.close();
 
-      let saida  = filtraVideos();
+      let saida = filtraVideos();
       escreveJsonVideos(saida);
     }
   });
